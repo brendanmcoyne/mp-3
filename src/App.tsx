1699fc6f137f4ec27aca7818ea466a9d1772b161
brendanmcoyne <bmcoyne@bu.edu>
2025-03-07 02:01:46 -0500

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { styled } from 'styled-components';
-import Header from './components/Header';
-import About from './components/About';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
-import Activities from './components/Activities';
-import Coursework from './components/Coursework';
-import Education from './components/Education';
-import Experience from './components/Experience';
-import Footer from './components/Footer';
-import Nav from './components/Nav';
+import { styled } from "styled-components";
+import Header from "./components/Header";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Activities from "./components/Activities";
+import Coursework from "./components/Coursework";
+import Education from "./components/Education";
+import Experience from "./components/Experience";
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
 import { createBrowserRouter, Route, RouterProvider, Routes } from "react-router";
 
 const MainContent = styled.div`
@@ -16,14 +16,18 @@ const MainContent = styled.div`
     flex-direction: row;
     justify-content: flex-start;
     align-items: stretch;
-    height: calc(100vh); 
+    height: 100vh;
 `;
 
 const Content = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: flex-start;
-    width: 70%;
+    width: 70%; /* Default: max 70% width */
+
+    @media (max-width: 1000px) {
+        width: 100%; /* Full width at 1000px or smaller */
+    }
 `;
 
 const Wrapper = styled.div`
@@ -31,45 +35,23 @@ const Wrapper = styled.div`
     margin: 0 auto;
 `;
 
-
 function Root() {
     return (
         <Wrapper>
             <Header />
             <MainContent>
-                <Nav/>
-            <Content>
-                <Routes>
-                    <Route
-                        path="/"
-                        element={<About/>}
-                    />
-                    <Route
-                        path="/activities"
-                        element={<Activities/>}
-                    />
-                    <Route
-                        path="/coursework"
-                        element={<Coursework/>}
-                    />
-                    <Route
-                        path="/education"
-                        element={<Education/>}
-                    />
-                    <Route
-                        path="/experience"
-                        element={<Experience/>}
-                    />
-                    <Route
-                        path="/projects"
-                        element={<Projects/>}
-                    />
-                    <Route
-                        path="/skills"
-                        element={<Skills/>}
-                    />
-                </Routes>
-            </Content>
+                <Nav />
+                <Content>
+                    <Routes>
+                        <Route path="/" element={<About />} />
+                        <Route path="/activities" element={<Activities />} />
+                        <Route path="/coursework" element={<Coursework />} />
+                        <Route path="/education" element={<Education />} />
+                        <Route path="/experience" element={<Experience />} />
+                        <Route path="/projects" element={<Projects />} />
+                        <Route path="/skills" element={<Skills />} />
+                    </Routes>
+                </Content>
             </MainContent>
             <Footer />
         </Wrapper>
@@ -77,7 +59,6 @@ function Root() {
 }
 
 const router = createBrowserRouter([{ path: "*", Component: Root }]);
-
 
 export default function App() {
     return <RouterProvider router={router} />;
